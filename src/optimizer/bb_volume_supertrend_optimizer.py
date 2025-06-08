8ec34337233804cc770c562580e2e3114117c77c
Alex Kosyrev <akossyrev@gmail.com>
2025-06-08 23:17:02 +0200

    xxx

--- src/optimizer/bb_volume_supertrend_optimizer.py
+++ src/optimizer/bb_volume_supertrend_optimizer.py
@@ -21,6 +21,7 @@ sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "../..")
 
 import json
 import warnings
+import traceback
 from typing import Any, Dict, Optional
 
 import backtrader as bt
